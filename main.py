1f8253753dd6a3807637761c7b441b6b051fb6c1
urdrops <urdrops@users.noreply.github.com>
2022-10-27 22:09:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply("Hello!\nWelcome to test bot! \nThis bot testing list of lessons for amity students!")
 
 
 
