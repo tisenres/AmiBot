c87983ebe45f1ce8146584e699f7f5afa4f1154d
Nastya <nastywushu@gmail.com>
2022-11-18 01:27:44 +0500

    create decoder

--- decoder.py
+++ decoder.py
@@ -1,7 +1,12 @@
+import re
+
 from schedule_processor import PeriodType
 
+DATE_EXTEND = "\\d{4}/\\d{2}/\\d{2}"
+TIME_SIMPLE = "\\d{2}:\\d{2}"
 
-def decode_json(array, period):
+
+def decode_json(array: [], period: str):
     list_of_lessons = []
     
     match PeriodType.get_by_value(period):
@@ -15,14 +20,41 @@ def decode_json(array, period):
     return list_of_lessons
 
 
-def decode_oneday_format(array):
+def decode_oneday_format(array: []):
     one_day_list = []
     list_of_lessons = []
+    
     for item in array:
-        one_day_list.append(f'<b>{item["start"]}</b> — <b>{item["title"]}</b>')
+        
+        start_time = re.findall(TIME_SIMPLE, item["start"])[0]
+        end_time = re.findall(TIME_SIMPLE, item["end"])[0]
+        
+        one_day_list.append(f'<b>{start_time} - {end_time}</b>   {item["title"]}')
+        
     list_of_lessons.append('\n'.join(one_day_list))
     return list_of_lessons
 
 
-def decode_week_format(array):
-    return []
+def decode_week_format(array: []):
+    one_day_list = []
+    list_of_lessons = []
+    previous_date = re.findall(DATE_EXTEND, array[0]['start'])
+    
+    for i in range(len(array)):
+        
+        item = array[i]
+        
+        current_date = re.findall(DATE_EXTEND, item['start'])
+        start_time = re.findall(TIME_SIMPLE, item["start"])[0]
+        end_time = re.findall(TIME_SIMPLE, item["end"])[0]
+        
+        if previous_date != current_date:
+            
+            previous_date = re.findall(DATE_EXTEND, item['start'])
+            list_of_lessons.append('\n'.join(one_day_list))
+            one_day_list = []
+            
+        one_day_list.append(f'<b>{start_time} - {end_time}</b>   {item["title"]}')
+        
+    list_of_lessons.append('\n'.join(one_day_list))
+    return list_of_lessons
