014020877f6290fe22339724540040b376c4447b
Nastya <nastywushu@gmail.com>
2022-11-21 20:23:44 +0500

    new token

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-TOKEN = '5447230707:AAGP85mG_yrhxGUhcCO7XVsoMw9_ClO1HwQ'
+TOKEN = '5961574704:AAHn3hL1Qe7sXLjWhUYSgO0FnlZdpQe1Jng'
+# @tashkent_amibot
+
+# TOKEN = '5447230707:AAGP85mG_yrhxGUhcCO7XVsoMw9_ClO1HwQ'
+# @testing_urdbot
